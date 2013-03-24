750918a4028706676f1f8a68d5acf36c3c7ae905
charles <cooper.charles.m@gmail.com>
2013-03-23 19:53:57 -0700

    added license

--- src/com/idylwood/Statistics.java
+++ src/com/idylwood/Statistics.java
@@ -1,3 +1,28 @@
+/*
+ * ====================================================
+ * Copyright (C) 2013 by Idylwood Technologies, LLC. All rights reserved.
+ *
+ * Developed at Idylwood Technologies, LLC.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice 
+ * is preserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * The License should have been distributed to you with the source tree.
+ * If not, it can be found at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Author: Charles Cooper
+ * Date: 2013
+ * ====================================================
+ */
 package com.idylytics;
 
 import org.apache.commons.math3.stat.regression.*;
