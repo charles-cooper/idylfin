charles-cooper/idylfin

commit 31063933fc0ce54165c210e22a12cb6628e34e0c

mersenne twister random

src/com/idylwood/utils/MathUtils.java
@@ -29,6 +29,8 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.Arrays;
 
+import ec.util.MersenneTwisterFast;
+
 // This class contains a bunch of numerical methods which are intended to be fast and precise.
 // Several of the methods come in 'fast', 'normal' and 'slow' versions to give the end user
 // more control over the trade-off between accuracy and speed.
@@ -382,15 +384,24 @@ public final class MathUtils {
 	}
 
 	// returns newly allocated array of length len
-	// whose elements are doubles between 0.0 and 1.0
-	// generated by java.lang.Math.random()
-	public static final double[] random(int len)
+	// whose elements are doubles uniformaly distributed between 0.0 and 1.0
+	// generated by MathUtils.random()
+	public static final double[] random(final int len)
 	{
 		final double ret[] = new double[len];
-		for (int i = len; i-- != 0; )
-			ret[i] = Math.random();
+		for (int i = 0; i < len; i++)
+			ret[i] = random();
 		return ret;
 	}
+
+	private static final MersenneTwisterFast random = new MersenneTwisterFast();
+	// not thread safe
+	// returns a double uniformly distributed between 0.0 and 1.0.
+	// The implementation may change without warning, currently uses the Mersenne Twister.
+	public static final double random()
+	{
+		return random.nextDouble();
+	};
 
 	// returns newly allocated array with same length as input
 	// elements are max(input,0)
@@ -418,6 +429,14 @@ public final class MathUtils {
 		return new org.apache.commons.math3.stat.descriptive.summary.Sum().evaluate(values);
 	}
 
+	public static double copySign(final double magnitude, final double sign)
+	{
+		final long m = Double.doubleToLongBits(magnitude);
+		final long s = Double.doubleToLongBits(sign);
+		if (0<=(m^s)) 
+			return -magnitude;
+		return magnitude; // flip sign
+	}
 	public static final double abs(final double d)
 	{
 		return Double.longBitsToDouble(Long.MAX_VALUE & Double.doubleToRawLongBits(d));
@@ -447,6 +466,8 @@ public final class MathUtils {
 	// This is much faster than Schewchuk's algorithm but not
 	// guaranteed to be perfectly precise
 	// In most cases, however, it is just as precise.
+	// Due to optimization it is about 30% faster
+	// than the naive implementation on my machine.
 	public static final double sum(final double... values)
 	{
 		double sum = 0;
@@ -477,17 +498,29 @@ public final class MathUtils {
 		return sum - err;
 	}
 
-	// Naive implementation of sum which is faster than MathUtils.sum().
+	// Numerically naive, unoptimized version of sum. Intended for demonstrating superiority of
+	// other methods only.
+	public static final double sumNaive(final double ... values)
+	{
+		double ret = 0;
+		for (final double x : values)
+			ret += x;
+		return ret;
+	}
+
+	// Numerically naive implementation of sum which is faster than MathUtils.sum() and sumNaive()
 	// Generally exhibits rounding error which grows with the length of the sum
 	// Note that it may not agree with other implementations
 	// due to optimizations which change the order of iteration
 	// which can affect the rounding error.
 	// It is O(n) in the length of the array to be summed.
+	// It is faster than the naive, unoptimized implementation by 20-40%
+	// (dependent on the mood of the JIT) on my machine.
 	public static final double sumFast(final double... values)
 	{
 		double ret = 0;
 		// unroll the loop since the JIT shouldn't
-		final int unroll = 4; // empirically unrolling more than 2 doesn't help much
+		final int unroll = 4; // empirically unrolling more than 3 doesn't help much
 		final int len = values.length - values.length%unroll;
 		int i = 0;
 		for (; i < len; i+=unroll)
@@ -571,6 +604,9 @@ public final class MathUtils {
 		if (startOne + len > x.length || startTwo + len > y.length)
 			throw new ArrayIndexOutOfBoundsException("Vector indices don't make sense!");
 		final int unroll = 3; // don't blindly change this without changing the loop!
+		// unroll was tuned to my machine. the optimal value is
+		// probably architecture specific and probably depends on
+		// the number of CPU registers
 		final int modLen = len - len%unroll;
 		double sum = 0;
 		double err = 0;
@@ -683,7 +719,9 @@ public final class MathUtils {
 		final Matrix ret = new Matrix(first.rows(), second.cols());
 		for (int i = 0; i < second.cols(); i++)
 		{
-			final double vector[] = second.extractColumn(i);
+			// extract the column beforehand to improve cache hits
+			// TODO think about extracting on fly to save cost of read
+			final double vector[] = new double[second.rows()];//second.extractColumn(i);
 			for (int j = 0; j < first.rows(); j++)
 			{
 				final double val = linearCombination(first.data(), vector, j*first.cols(), 0, first.cols());
@@ -713,8 +751,9 @@ public final class MathUtils {
 			throw new ArrayIndexOutOfBoundsException("Trying to multiply matrices of different dimensions?!");
 		final double ret[][] = new double[firstRows][secondCols];
 		for (int i = 0; i < secondCols; i++)
-			// iterate over columns so we can maintain cache locality!
+		// iterate over columns so we can maintain cache locality!
 		{
+			// TODO think about extracting column on the fly
 			final double[] vector = extractColumn(second,i);
 			for (int k = 0; k < firstRows; k++)
 			{
@@ -833,7 +872,7 @@ public final class MathUtils {
 	}
 
 	// Faster but lower precision than linearCombination.
-	// Naive implementation.
+	// Numerically naive implementation.
 	public static final double linearCombinationFast(final double[]x, final double[]y)
 	{
 		if (x.length!=y.length)
@@ -855,6 +894,7 @@ public final class MathUtils {
 			ret+= x[xPtr]*y[yPtr]
 				+ x[xPtr+1]*y[yPtr+1]
 				+ x[xPtr+2]*y[yPtr+2];
+				//+ x[xPtr+3]*y[yPtr+3];
 		}
 		// get the terms at the end
 		for (; i < len; i++)
@@ -962,26 +1002,33 @@ public final class MathUtils {
 
 	public static void main(String[] args)
 	{
-		final double [][]cov = covariance(new double[][]{{1,2,3},{0,1,1.5}});
-		System.out.println(cov[1][1] - 0.5833333333333333703408);
-		if (true) return;
 
-		final int len = 1000*1000*100;
-		final double[] data = random(len);
-		final double fast,medium,slow;
+		logTime("start");
+		final int len = 1000*1000*10;
+		final double[] data = shift(random(len),-.5);
+		logTime("random");
+		double fast,medium,slow;
+		fast = medium = slow = 0;
 
-		fast = sumFast(data);
-		slow = sumSlow(data);
-		medium = sum(data);
+		for (int i = 0; i < 100; i++)
+		{
+			fast = sumFast(data);
+			slow = sum(data);
+			medium = sumNaive(data);
+		}
 		compare("FS",fast,slow);
 		compare("MS",medium,slow);
 		compare("FM",fast,medium);
 
-		sumSlow(data);
+		logTime("warmup");
+		for (int i = 0; i < 100; i++)
+			sum(data);
 		logTime("slow");
-		sumFast(data);
+		for (int i = 0; i < 100; i++)
+			sumFast(data);
 		logTime("fast");
-		sum(data);
+		for (int i = 0; i < 100; i++)
+			sumNaive(data);
 		logTime("mine");
 	}
 }

README
@@ -8,7 +8,7 @@ To provide a wrapper to Yahoo Finance API as well as a library consisting of fin
 
 Features:
 Pure 100% Java implementation which is fast _and_ numerically stable
-Fast -- e.g. matrix vector multiplication is 10% faster than OpenGamma BLAS port to Java
+Fast -- e.g. matrix vector multiplication which is 10% faster than OpenGamma's Java BLAS implementation
 Numerically stable -- no other Java numerics library is implemented with numerical stability in mind
 Thread safe design
 Lightweight
@@ -18,12 +18,12 @@ Sharpe Ratio and other financial statistics
 Split and dividend adjustment
 
 Dependencies:
-IdylFin currently depends on Apache Commons Math, Jsoup and GRAL, forks of which are included in this source tree. In future versions the dependencies may be removed so that the code is self contained.
+IdylFin currently depends on Apache Commons Math, sections of OpenGamma, Jsoup and GRAL, forks of which are included in this source tree. In future versions the dependencies may be removed so that the code is self contained.
 
 License:
 Apache 2.0. You can view the terms of the license at http://www.apache.org/licenses/LICENSE-2.0.html. Idylwood Technologies is not responsible for any damages resulting from the use or misuse of this code.
 
-Lead Commit:
+Lead Contributor:
 Charles Cooper
 
 Contributors:

TODO
@@ -2,4 +2,5 @@ Minimize dependencies
 Options data / pricing
 Portfolio optimization
 Write javadoc
+Implement BLAS API
 

src/Foo.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import org.apache.commons.math3.util.FastMath;
 
 import com.idylwood.utils.MathUtils;
@@ -70,7 +72,7 @@ public class Foo {
 	public static void main(String [] args)
 	{
 		final Random r = new Random();
-		final int len = 1000*2;
+		final int len = 1000*1;
 		final double[][] one = new double[len][];
 		final double[][] two = new double[len][];
 		for (int i = 0; i < len; i++)
@@ -78,18 +80,32 @@ public class Foo {
 			one[i] = MathUtils.random(len);
 			two[i] = MathUtils.random(len);
 		}
+		/*
 		final Matrix mOne = new Matrix(one);
 		final Matrix mTwo = new Matrix(two);
 		MathUtils.matrixMultiply(mOne,mTwo);
 		final DenseMatrix dmOne = new DenseMatrix(one);
 		DenseMatrix dmTwo = new DenseMatrix(two);
+		*/
 		logTime("Start");
+		double [] foo;
 		for (int i = 0; i < len; i++)
 		{
-			MathUtils.matrixMultiplyFast(mOne,two[i]);
+			//MathUtils.matrixMultiplyFast(mOne,two[i]);
 			//BLAS2.dgemv(dmOne,two[i]);
+			//mOne.extractColumn(i);
+			//foo = new double[len];
+			//Arrays.fill(foo,i);
+			MathUtils.sumFast(two[i]);
+			MathUtils.sumNaive(one[i]);
 		}
+		logTime("warmed up");
+		for (int i = 0; i < len; i++)
+			MathUtils.sumFast(one[i]);
 		logTime("one");
+		for (int i = 0; i < len; i++)
+			MathUtils.sumNaive(two[i]);
+		logTime("two");
 
 		if (true) return;
 

src/com/idylwood/yahoo/YahooFinance.java
@@ -351,8 +351,10 @@ public class YahooFinance
 
 	static long time = 0;
 	// helper function
-	static public void logTime(String msg) {
-		final long newTime = System.currentTimeMillis();
+	static public void logTime(String msg)
+	{
+		//final long newTime = System.currentTimeMillis();
+		final long newTime = System.nanoTime();
 		final long elapsed = newTime - time;
 		System.out.println(msg+" "+elapsed);
 		time = newTime;

commit 3e628c0074b67c1e9c9772cbb7b6f86d3f846f41

fix matrix multiplication bug introduced in 31063933fc0ce54165c210e22a12cb6628e34e0c

src/com/idylwood/utils/MathUtils.java
@@ -721,7 +721,7 @@ public final class MathUtils {
 		{
 			// extract the column beforehand to improve cache hits
 			// TODO think about extracting on fly to save cost of read
-			final double vector[] = new double[second.rows()];//second.extractColumn(i);
+			final double vector[] = second.extractColumn(i);
 			for (int j = 0; j < first.rows(); j++)
 			{
 				final double val = linearCombination(first.data(), vector, j*first.cols(), 0, first.cols());
@@ -740,7 +740,7 @@ public final class MathUtils {
 		{
 			// extract the column beforehand to improve cache hits
 			// TODO think about extracting on fly to save cost of read
-			final double vector[] = new double[second.rows()];//second.extractColumn(i);
+			final double vector[] = second.extractColumn(i);
 			for (int j = 0; j < first.rows(); j++)
 			{
 				final double val = linearCombinationFast(first.data(), vector, j*first.cols(), 0, first.cols());